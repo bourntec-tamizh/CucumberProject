41485bc910c98e505fdfe3878de7999a1c5a241c
tamizh-bourntec <104750080+tamizh-bourntec@users.noreply.github.com>
2022-07-20 10:33:31 +0530

    previous change reverted

--- src/main/java/dataProvider/ConfigReader.java
+++ src/main/java/dataProvider/ConfigReader.java
@@ -1,37 +1,15 @@
 package dataProvider;
 
-import java.io.BufferedReader;
-import java.io.FileNotFoundException;
-import java.io.FileReader;
+import java.io.FileInputStream;
 import java.io.IOException;
 import java.util.Properties;
 
 public class ConfigReader {
-    private Properties properties;
-    private final String propertyFilePath= "config//config.properties";
 
-
-    public ConfigReader(){
-        BufferedReader reader;
-        try {
-            reader = new BufferedReader(new FileReader(propertyFilePath));
-            properties = new Properties();
-            try {
-                properties.load(reader);
-                reader.close();
-            } catch (IOException e) {
-                e.printStackTrace();
-            }
-        } catch (FileNotFoundException e) {
-            e.printStackTrace();
-            throw new RuntimeException("Configuration.properties not found at " + propertyFilePath);
-        }
-    }
-
-    public  String getConfigValue(String propertName) {
-        String property = properties.getProperty(propertName);
-        if (property != null) return property;
-        else throw new RuntimeException("property not specified in the Configuration.properties file.");
+    public static String getConfigValue(String key) throws IOException {
+        Properties prop = new Properties();
+        FileInputStream fis = new FileInputStream(System.getProperty("user.dir") + "//config//config.properties");
+        prop.load(fis);
+        return prop.getProperty(key);
     }
 }
-
--- src/test/java/runners/TestRunner.java
+++ src/test/java/runners/TestRunner.java
@@ -2,6 +2,7 @@ package runners;
 
 import com.aventstack.extentreports.ExtentReports;
 import com.aventstack.extentreports.reporter.ExtentHtmlReporter;
+import dataProvider.ConfigReader;
 import io.cucumber.testng.AbstractTestNGCucumberTests;
 import io.cucumber.testng.CucumberOptions;
 import org.testng.annotations.*;
